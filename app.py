927056c42130714cccc6a817f30ebb83d153b101
vathumy <vathumyv@gmail.com>
2026-05-23 20:54:12 +0530

    Student model created

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -7,3 +8,23 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root123@localhost/
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
+class Student(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+
+    cgpa = db.Column(db.Float, default=0.0)
+
+    is_active = db.Column(db.Boolean, default=True)
+
+    joined_date = db.Column(db.Date, nullable=False)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    
+
+
+
