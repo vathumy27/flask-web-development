84e0910301f5d202770051069e31114b2e309d30
vathumy <vathumyv@gmail.com>
2026-05-26 15:34:10 +0530

    config

---
+++ app/config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://{os.getenv('DB_USER')}:"
+        f"{os.getenv('DB_PASSWORD')}@"
+        f"{os.getenv('DB_HOST')}:"
+        f"{os.getenv('DB_PORT')}/"
+        f"{os.getenv('DB_NAME')}"
+    )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
